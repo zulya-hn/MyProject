20c8746ec5c856de77c9eeba4e21c20f79b3ac62
zulya <zulya.h95@gmail.com>
2020-02-12 13:22:59 +0300

    Done

--- js/app.js
+++ js/app.js
@@ -4,24 +4,66 @@
 $(function () {
     
     let header = $("#header"),
-        welcomeH = $("#welcome").innerHeight(),
+        welcomeH = $("#welcome").innerHeight() - 30,
         scrollOffset = 0;
     
+    // Fixed Header
     checkScroll(scrollOffset);
     
     $(window).on("scroll", function () {
         scrollOffset = $(this).scrollTop();
-    
+        
         checkScroll(scrollOffset);
     });
     
     function checkScroll(scrollOffset) {
-        if(scrollOffset >= welcomeH) {
+        if (scrollOffset >= welcomeH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     }
     
-});
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        let $this = $(this),
+            blockID = $(this).data('scroll'),
+            blockOffset = $(blockID).offset().top;
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+        
+    });
+    
+    
+    // Menu nav toggle (burger)
+    
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+        
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+        
+    });
+    
+    //accordion
+    
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+        
+        let $this = $(this),
+            blockId = $this.data('collapse');
+        
+        $this.toggleClass("active");
+        
+    });
+    
+    
+})
 

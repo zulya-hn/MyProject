e65b2cb661e9dba03aac53105fc0b7e8fba2d258
zulya <zulya.h95@gmail.com>
2020-03-15 19:50:00 +0300

    js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,63 @@
-
-
-
 $(function () {
+    fixedHeaderByScroll();
+    animateScroll();
+    toggleBurgerListener();
+    toggleAccordionListener();
+    setNavActiveByScroll();
+});
+
+// Menu nav toggle (burger)
+function toggleBurgerListener() {
     
-    let header = $("#header"),
-        welcomeH = $("#welcome").innerHeight() - 30,
+    $('#nav_toggle').on('click', function (event) {
+        event.preventDefault();
+        
+        $(this).toggleClass('active');
+        $('#nav').toggleClass('active');
+        
+    });
+}
+
+//accordion
+function toggleAccordionListener() {
+    
+    $('[data-collapse]').on('click', function (event) {
+        event.preventDefault();
+        
+        let $this = $(this),
+            blockId = $this.data('collapse');
+        
+        $this.toggleClass('active');
+        
+    });
+}
+
+// Smooth scroll
+function animateScroll() {
+    
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+        
+        let $this = $(this),
+            blockID = $(this).data('scroll'),
+            blockOffset = $(blockID).offset().top - 30;
+        
+        $('html, body').animate({
+            scrollTop: blockOffset
+        }, 500);
+        
+    });
+}
+
+function fixedHeaderByScroll() {
+    let header = $('#header'),
+        welcomeH = $('#welcome').innerHeight() - 30,
         scrollOffset = 0;
     
     // Fixed Header
     checkScroll(scrollOffset);
     
-    $(window).on("scroll", function () {
+    $(window).on('scroll', function () {
         scrollOffset = $(this).scrollTop();
         
         checkScroll(scrollOffset);
@@ -18,52 +65,28 @@ $(function () {
     
     function checkScroll(scrollOffset) {
         if (scrollOffset >= welcomeH) {
-            header.addClass("fixed");
+            header.addClass('fixed');
         } else {
-            header.removeClass("fixed");
+            header.removeClass('fixed');
         }
     }
-    
-    // Smooth scroll
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
-        
-        let $this = $(this),
-            blockID = $(this).data('scroll'),
-            blockOffset = $(blockID).offset().top;
-        
-        $("#nav a").removeClass("active");
-        $this.addClass("active");
-        
-        $("html, body").animate({
-            scrollTop: blockOffset
-        }, 500)
-        
-    });
-    
-    
-    // Menu nav toggle (burger)
-    
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
-        
-        $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
-        
-    });
-    
-    //accordion
-    
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-        
-        let $this = $(this),
-            blockId = $this.data('collapse');
-        
-        $this.toggleClass("active");
-        
-    });
-    
-    
-})
+}
 
+function setNavActiveByScroll() {
+    
+    $(window).on('scroll', () => {
+        let headerHeight = 49;
+        let middleOfScreen = document.documentElement.clientWidth / 2;
+        
+        let currentSection = document
+            .elementFromPoint(middleOfScreen, headerHeight)
+            .closest('section[id]');
+    
+        if (currentSection) {
+            let currentSectionName = $(currentSection).attr('id');
+            
+            $('.nav__link').removeClass('active');
+            $(`[data-scroll="#${currentSectionName}"]`).addClass('active');
+        }
+    });
+}
